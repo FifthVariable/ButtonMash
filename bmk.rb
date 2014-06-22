dfad777ee31d7262b65572f1d3a2a85a70475a14
FifthVariable <nlec1901@mpsedu.org>
2014-06-22 10:32:52 -0500

    add graphics, title screen, and other minor fixes

--- bmk.rb
+++ bmk.rb
@@ -59,18 +59,20 @@ end
 class ButtonMash
   HIGHSCORE_FILENAME = 'highscore'
   DEFAULT_DIFFICULTY = "1"
-  DIFFICULTY_TIME = { "no" => 40, "maybe" => 30, "kinda" => 20, "yes" => 10, "hacks" => 1 }
+  DIFFICULTY_TIME = { "no" => 40, "maybe" => 30, "kinda" => 20, "yes" => 10, "hacks" => 1}
   DIFFICULTY_BUTTONS = { "2" => 2.times.map { (65 + rand(26)).chr.downcase },
                          "3" => 3.times.map { (65 + rand(26)).chr.downcase },
                          "4" => 4.times.map { (65 + rand(26)).chr.downcase },
                          "5" => 5.times.map { (65 + rand(26)).chr.downcase },
-                         "6" => 6.times.map { (65 + rand(26)).chr.downcase }}
-  MUSIC_CHOICE = { "country"    => "Media/cruise.mp3",
-                   "pop"        => "Media/happy.mp3",
-                   "rap"        => "Media/sway.mp3",
-                   "dubstep"     => "Media/bass.mp3",
-                   "baby metal" => "Media/baby.mp3",
-                   "chiptune"   => "Media/chiptune.mp3" }
+                         "6" => 6.times.map { (65 + rand(26)).chr.downcase },
+						 "10"=>10.times.map { (65 + rand(26)).chr.downcase },
+						 "20"=>20.times.map { (65 + rand(26)).chr.downcase }}
+  #MUSIC_CHOICE = { "country"    => "Media/cruise.mp3",
+                   #"pop"        => "Media/happy.mp3",
+                   #"rap"        => "Media/sway.mp3",
+                   #"dubstep"     => "Media/bass.mp3",
+                   #"baby metal" => "Media/baby.mp3",
+                   #"chiptune"   => "Media/chiptune.mp3" }
 
   attr_reader :count
   attr_reader :name
@@ -78,6 +80,7 @@ class ButtonMash
 
 
   def initialize
+	@numb = 0
     @count = 0
     @score_keeper = ScoreKeeper.new(HIGHSCORE_FILENAME)
   end
@@ -87,27 +90,31 @@ class ButtonMash
   end
 
   def show_countdown
+  
     puts "COUNTDOWN COMMENCING!"
     sleep 1
     clear
-    puts "5"
+    puts "5.376432"
     sleep 1
     clear
-    puts "4"
+    puts "4.356892"
     sleep 1
     clear
-    puts "3"
+    puts "3.14159265358979323846264338327950"
+    sleep 1.5
+    clear
+    puts "2.283475"
     sleep 1
     clear
-    puts "2"
+    puts "1.9823047"
     sleep 1
     clear
-    puts "1"
-    sleep 1
-    clear
-    puts "Go!"
-    sleep 0.4
+	puts "GO!!!"
+	sleep 1
+			
+	
   end
+  
   def show_signup
     if @signup == "yes"
       puts "Welcome Newbie! Please Input A Username: "
@@ -121,14 +128,26 @@ class ButtonMash
       clear
     end
   end
+
   def show_intro
     clear
-
-    puts "For Years, The World Has Been Searching For A ButtonMash King...Will It Be You?"
-    puts "Are you new to the Mash? yes,no"
+	puts "LET'S EAT (,) GRANDMA PRODUCTIONS"
+	puts "PRESENTS:"
+	sleep 3.5
+	puts "                                                                                 
+                                             
+	| ------------------------------------ |				                     
+	| BUTTON MASH KINGDOMS: 1N5ANE ED1T10N |                                     
+	| ------------------------------------ |            
+											  "
+	sleep 4.3
+	clear
+    puts "Press,Push,Smash,Mash! Play Until You Get Carpal Tunnel!"
+	puts " (please don't...)"
+    puts "Are you new to the Kingdom? *yes,no*"
     @signup = gets.strip
     show_signup
-    puts "Do you even mash? no,maybe,kinda,yes"
+    puts "Do you even mash? *no,maybe,kinda,yes*"
   end
 
   def show_finalscore
@@ -146,7 +165,7 @@ class ButtonMash
     Curses.stdscr.keypad(true) # enable arrow keys (required for pageup/down)
   end
   def button_selection
-    puts "How Many Fingers Do You Have? 2,3,4,5,6"
+    puts "How Many Fingers Can You Mash With, Bro? *2,3,4,5,6,10,20*"
     @finger = gets.strip
   end
 
@@ -167,19 +186,22 @@ class ButtonMash
   end
 
   def check_correct
+	clear
     puts "Name:#{@name}"
     puts "Difficulty: #{@diff}"
     puts "Music: #{@choice}"
     puts "Your Keys Are: #{button_mash_list.join(", ")}"
-    puts "IS THIS GOOD? yes,no"
+    puts "IS THIS GOOD? *yes,no*"
     gets.strip == "yes"
   end
 
 
   def music_taste
+	clear
     puts "What Kind Of Music Do You Like?"
-    puts "country,pop,rap,dubstep,baby metal"
+    puts "*country,pop,rap,dubstep,baby metal*"
     @choice = gets.strip
+	clear
   end
 
 
@@ -192,14 +214,15 @@ class ButtonMash
   end
 
   def run!
-    music_taste
-    @music = Gosu::Sample.new(Gosu::Window.new(100,100,false), MUSIC_CHOICE[@choice] )
-    @music.play
+    #music_taste
+    #@music = Gosu::Sample.new(Gosu::Window.new(100,100,false), MUSIC_CHOICE[@choice] )
+    #@music.play
     show_intro
 
     #button
 
     @diff = gets.strip
+	easter_egg
 
     button_selection
     run! if !check_correct
@@ -217,6 +240,7 @@ class ButtonMash
       puts "    New HighScore!"
       score_keeper.set_score(name, count)
     end
+	clear
 
     score_keeper.save_scores
     show_finalscore
@@ -226,9 +250,14 @@ class ButtonMash
     Curses.setpos(0,0)
     Curses.addstr("#{count}")
   end
+  
+  def easter_egg
+		
+	end
 
   def clear
     system("clear")
+	system("cls")
   end
 end
 
